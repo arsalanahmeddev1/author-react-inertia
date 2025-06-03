923ef5ead814871da8554522ec062da295fef1e2
unknown <production8411@gmail.com>
2025-06-04 03:56:44 +0500

    local changes

--- author_web.sql
+++ author_web.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
 -- Generation Time: Jun 03, 2025 at 06:08 PM
-=======
--- Generation Time: Jun 03, 2025 at 06:04 PM
->>>>>>> feature/seprating-standard-community
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
