cd7d016cc66fac14a93192c61d2ff679491b9fdf
legacy-prod <support@americandigitalagency.us>
2025-07-30 22:27:35 +0500

    tailwind issue solved

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.jsx',
+//             refresh: true,
+//         }),
+//         react(),
+//     ],
+// });
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-});
+  plugins: [
+    laravel({
+        input: 'resources/js/app.jsx',
+        refresh: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+})
